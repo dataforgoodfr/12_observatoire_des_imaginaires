442f08d33f3ef6aebc088ef7116f9b303ed9a125
Luis Arias <luis@inclusif.eu>
2024-03-06 18:33:45 +0100

    Hide kaggle output to stdout

--- site-observable/docs/data/movies.csv.sh
+++ site-observable/docs/data/movies.csv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 temp_dir=$(mktemp -d)
 cd "$temp_dir"
-kaggle datasets download -d asaniczka/tmdb-movies-dataset-2023-930k-movies
+kaggle datasets download -d asaniczka/tmdb-movies-dataset-2023-930k-movies >&2
 unzip tmdb-movies-dataset-2023-930k-movies.zip >&2
 cat TMDB_movie_dataset_v11.csv
--- site-observable/docs/data/shows.csv.sh
+++ site-observable/docs/data/shows.csv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 temp_dir=$(mktemp -d)
 cd "$temp_dir"
-kaggle datasets download -d asaniczka/full-tmdb-tv-shows-dataset-2023-150k-shows
+kaggle datasets download -d asaniczka/full-tmdb-tv-shows-dataset-2023-150k-shows >&2
 unzip full-tmdb-tv-shows-dataset-2023-150k-shows.zip >&2
 cat TMDB_tv_dataset_v3.csv
